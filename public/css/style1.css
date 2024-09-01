3dbfd7819f1b23494f7067ba5641f877d0a91947
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-09-01 17:47:55 +0530

    Create style1.css

---
+++ public/css/style1.css
@@ -0,0 +1,77 @@
+
+h1{
+    color:rgb(5, 84, 133);
+   
+   font-family:'Times New Roman', Times, serif, Helvetica, sans-serif, Courier, monospace, Geneva, Tahoma, sans-serif, 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif, Tahoma, Geneva, Verdana, sans-serif, 'Arial Narrow', Arial, sans-serif, 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.body1{
+
+    background-image: url(https://jdsportsblog.s3.amazonaws.com/wp-content/uploads/2020/03/Background_Plate_1376x467.jpg);
+    background-repeat:no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 55%;
+    opacity: 50px;
+    
+}
+th
+{
+    color:rgb(95, 131, 179);
+}
+.button{
+    display: inline-block;
+    border-radius: 5px;
+    border: 2px double green;
+    color:  green;
+    text-align: center;
+    font-size: 15px;
+    padding: 10px;
+    width: 170px;
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    -o-transition: all 0.5s;
+    transition: all 0.5s;
+    cursor: pointer;
+    
+    margin:0 auto;
+    display:block
+}
+.delete{
+    display: inline-block;
+    border-radius: 5px;
+    border: 2px double  red;
+    color:red;
+    text-align: center;
+    font-size: 15px;
+    padding: 10px;
+    width: 170px;
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    -o-transition: all 0.5s;
+    transition: all 0.5s;
+    cursor: pointer;
+    
+    margin-left:38.5%;
+    display:block
+}
+.cat{
+    font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    font-size: 30;
+    
+}
+#img{
+    align-items: center;
+}
+.table2{
+    margin-left: auto;
+    margin-right: auto;
+}
+.box1{
+    border:2px solid rgb(233, 216, 216) ;
+    background-color:rgb(235, 215, 215);
+    width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+}
+  
+  
